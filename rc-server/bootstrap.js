444d9415e342bea44c9c52c8b622beedc48d910b
Liam <urehelonn@gmail.com>
2019-05-21 02:21:44 -0400

    docker setup, mysql removed

---
+++ rc-server/bootstrap.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize('think_and_read', 'root', 'example', {
+  host: '127.0.0.1',
+  dialect: 'mysql'
+});
+
+const Users = sequelize.define('users', {
+  id: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+Users.sync({ force: true }).then(() => {
+  // Now the `users` table in the database corresponds to the model definition
+  return Users.create({
+    id: 'qwe',
+    password: '12345'
+  });
+});
